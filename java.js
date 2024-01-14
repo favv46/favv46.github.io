07b0564f2666e606723feb528fbf8d5b73fa47c3
favv46 <75524831+favv46@users.noreply.github.com>
2024-01-14 13:33:34 +0100

    commit

--- java.js
+++ java.js
@@ -6,25 +6,3 @@ function onload() {
         window.location.href = "Home.html"
     }
     }
-
-/*
-const buttons = document.getElementsByClassName("buttons")
-for (let i = 0; i < buttons.length; i ++){
-    buttons[i].addEventListener("mouseenter", function (){
-        const h1 = buttons[i].querySelector("h1")
-        if (h1){
-            h1.style.fontFamily = "Integral_CF_Heavy"
-            h1.style.fontSize = "1.07vw"
-            h1.style.letterSpacing = "1px"
-        }
-    })
-    buttons[i].addEventListener("mouseleave", function (){
-        const h1 = buttons[i].querySelector("h1")
-        if (h1){
-            h1.style.fontFamily = "Snake"
-            h1.style.fontSize = "3"
-            h1.style.letterSpacing = "normal"
-        }
-    })
-}
-*/
