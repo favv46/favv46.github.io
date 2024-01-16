205c0d77efb1239a7da3e935dc9d56807697bcf2
favv46 <75524831+favv46@users.noreply.github.com>
2024-01-16 01:34:39 +0100

    commit

---
+++ img_buttons.js
@@ -0,0 +1,42 @@
+document.getElementById('button_1').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_3').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+  
+document.getElementById('button_4').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_5').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+
+
+document.getElementById('button_1_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_2_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_3_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+  
+document.getElementById('button_4_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+
+document.getElementById('button_5_2').onclick = function() {
+    window.location.href = 'Home.html';
+  };
+  
