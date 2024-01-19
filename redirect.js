e497017179bc7229dbcddc8914f9a8f17eba3590
favv46 <75524831+favv46@users.noreply.github.com>
2024-01-19 16:52:39 +0100

    commit

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 function onload() {
     if (navigator.userAgentData.mobile) {
-        window.location.href = "Home_mobile.html"
+        window.location.href = "Home.html"
     }
     else {
         window.location.href = "Home.html"
